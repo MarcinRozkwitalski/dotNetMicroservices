85ba4853ad30284a78a367d4f8ae40508438fd28
Marcin <marcinrozkwitalski98@gmail.com>
2024-05-09 23:03:13 +0200

    Project config for RabbitMQ

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1973682a2810f5bb0bf40b41814d65f19dfe00fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103cd19eaebcb66f9f633bef1e65821eddcfbdc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
