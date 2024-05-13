a3afe52fec4b07de0d2da72603b77fed9fbeb9ad
Marcin <marcinrozkwitalski98@gmail.com>
2024-05-13 23:51:03 +0200

    Final Kubernetes networking config (for GRPC)

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08933c0f841b099f5c24bb82a1af9ce2af196b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5761e43e04cb4fe20bffefdc50ed255a2254ce7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
