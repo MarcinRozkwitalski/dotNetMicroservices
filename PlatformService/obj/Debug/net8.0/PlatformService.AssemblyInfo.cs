c366448536b76468b9055d8bafcad14d4195bcf7
Marcin <marcinrozkwitalski98@gmail.com>
2024-05-14 22:48:24 +0200

    Created platforms proto file

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3afe52fec4b07de0d2da72603b77fed9fbeb9ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc2e982f524b77fd206e765efd511a9a0e5c691")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
